839dfae5797fe9a45eaefd6e6e320fbc8deb5e4c
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-03-17 13:08:07 -0400

    Changed the function so the 'alt' text of the clicked image was used instead of the src. This is clever but not the correct way to use alt text

--- interactive-gallery/main.js
+++ interactive-gallery/main.js
@@ -1,13 +1,14 @@
 let imgButtons = document.getElementsByClassName('gallery-image')
 let heroImage = document.getElementById('heroImg')
 
+
 Array.from(imgButtons).forEach((element) => {
     element.addEventListener('click', changeImage)
 })
 
 function changeImage() {
-    let newImage = this.src
-    heroImage.src=newImage
-    document.getElementById('lure-item-number').innerText=heroImage.src
+    let newImage = this
+    heroImage.src=newImage.src
+    document.getElementById('lure-item-number').innerText=newImage.alt
     document.getElementById('lure-notes').innerText='This is a test'
 }
