7568ecd8f6d358a908994ae706cc4a9e9ea3c978
kirk-esterline <kirk.esterline@gmail.com>
2024-03-19 14:03:25 -0400

    Attempted to wrap the images in a div to give more selectible elements. It did not work as intended but it needs some additional tinkering to get the styling and querySelectors correct.

--- interactive-gallery/main.js
+++ interactive-gallery/main.js
@@ -8,9 +8,8 @@ Array.from(imgButtons).forEach((element) => {
 
 function changeImage() {
     let newImage = this
-    let newText = document.getElementById('des1').innerHTML
     heroImage.src=newImage.src
-    document.getElementById('lure-item-number').innerText=newImage.alt
-    document.getElementById('lure-notes').appendChild=newText
+    document.getElementById('lure-item-number').innerText=newImage.span
+    document.getElementById('lure-notes').innerHTML=newImage.p
 }
 
