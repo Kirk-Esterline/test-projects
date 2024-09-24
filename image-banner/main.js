802c808cec96426e2b0169763f70f27a04037418
Kirk Esterline <kirk.esterline@gmail.com>
2024-09-24 16:32:40 -0400

    updated main and style JS with calculated padding in an attempt to get styles consistant accross all sizes. It has not worked thus far. It works in vanilla js, but not in WP.

--- image-banner/main.js
+++ image-banner/main.js
@@ -2,8 +2,11 @@ function resizeFont() {
     const parent = document.querySelector('figure')
     const child = document.querySelector('figcaption')
     const parentWidth = parent.offsetWidth
+    const parentHeight = parent.offsetHeight
     const newFontSize = parentWidth / 10
+    const newTextPadding = parentHeight / 50
     parent.style.setProperty('--font-size', `${newFontSize}px`)
+    parent.style.setProperty('--padding-top', `${newTextPadding}px`)
 }
 
 window.addEventListener('resize', resizeFont)
