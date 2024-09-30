67a75901b5e8421edbf4d8c170f9b3b758dd59b4
Kirk Esterline <kirk.esterline@gmail.com>
2024-09-30 16:46:35 -0400

    created my first C# and .NET test project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hi, what is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine("Hello " + name + ", nice to meet you, the current date and time are: " + currentDate);
