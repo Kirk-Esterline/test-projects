e16828471ce5bce2b34d398e95fc8397530ba3a9
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-08 13:48:01 -0400

    added an example of the final script which was added to wordpress.

--- interactive-gallery/main.js
+++ interactive-gallery/main.js
@@ -1,8 +1,8 @@
-let imgButtons = document.getElementsByClassName('gallery-image')
+let imgButtons = Array.from(document.querySelectorAll('.gallery img'))
 let heroImage = document.getElementById('heroImg')
 
 
-Array.from(imgButtons).forEach((element) => {
+imgButtons.forEach((element) => {
     element.addEventListener('click', changeImage)
 })
 
@@ -13,3 +13,23 @@ function changeImage() {
     document.getElementById('lure-notes').innerHTML=newImage.dataset.des
 }
 
+// Here is the final code that ended up working in WordPress
+
+document.addEventListener('DOMContentLoaded', () => { // an event listener waiting for the DOM to be ready before attempting to added the event listeners to elements.
+	let imgButtons = Array.from(document.querySelectorAll('.gallery-image-links img'))
+	let heroImage = document.querySelector('.heroImage')
+	let heroImageItemNumber = document.querySelector('.hero-image-item-number')
+	
+	imgButtons.forEach((element) => {
+    	element.addEventListener('click', changeImage)
+	})
+
+	function changeImage() {
+    	let newImage = this
+    	heroImage.src = newImage.src
+		heroImage.srcset = newImage.srcset
+		heroImageItemNumber.style.setProperty('display', 'block')
+		heroImageItemNumber.innerText = newImage.parentElement.querySelector('figcaption').innerText
+		document.querySelector('figcaption').style.setProperty('display', 'none')
+}
+})
