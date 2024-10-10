d6eb1075eb06f1af67bbaac40b41a39b3d8b142a
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-10 14:44:39 -0400

    started a new simple project to create a rock paper scissors game in the console

---
+++ console-rock-paper-scissors/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Rock Paper Scissors!");
+Console.WriteLine("If you are ready to play press any key to continue ... ");
+Console.ReadKey(true);
+Console.WriteLine("Type in your move, then press enter to 'Shoot'...");
+
+string move = Console.ReadLine();
+
+if (move == "rock")
+{
+    Console.WriteLine("Rock was a good choice");
+}
